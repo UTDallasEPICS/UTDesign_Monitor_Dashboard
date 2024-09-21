208edde8ed322c7a6e3d4f5f73825ff7a4eb1e28
harshptl05 <harshptl2023@gmail.com>
2024-09-21 16:34:09 -0500

    typescript changes

--- types.ts
+++ types.ts
@@ -5,3 +5,22 @@ export interface User {
     role: string;
     dashboards: Dashboard[];
   }
+
+export interface Dashboard {
+  cuid: string;
+  slides: Slide[];
+  owner: User;
+}
+
+export interface Slide {
+  index: string;
+  dashboard: Dashboard;
+  image: string;
+  video: string;
+  website: string;
+}
+
+export interface Device {
+  cuid: string;
+  name:string;
+}
