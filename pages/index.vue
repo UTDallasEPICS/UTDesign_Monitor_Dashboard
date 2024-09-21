988c03b6eb8df900c2ef4c18655452be26e36fee
fegelsamuel <fegelsamuel@gmail.com>
2024-09-21 18:37:14 -0500

    types.ts and index.vue updated to present

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
-<script lang="ts">
+<script setup lang="ts">
+
 
 </script>
 
-<template>
-    <div>
-        <h1>Hello, World</h1>
-    </div>
+<template lang="pug">
+div.mx-auto.mt-7.absolute
+  div.mx-auto.gap-2
+    button
+      NuxtLink(to="/") Home
+    button 
+      NuxtLink(to="/my-dashboards") my dashboards
+    button
+      NuxtLink(to="/shared-dashboards") shared dashboards
+    button
+      NuxtLink(to="/new-dashboard") new dashboard
+    button
+      NuxtLink(to="/admin") Admin
+NuxtPage
 </template>
