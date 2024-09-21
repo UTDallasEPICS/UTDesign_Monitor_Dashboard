23ca2e543542b22166c652232eed68675c7bab07
madeline <madeline.scogin@gmail.com>
2024-09-21 17:04:58 -0500

    anything

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,17 @@
 </script>
 
 <template lang="pug">
-div.max-w-min.mx-auto.mt-7.absolute
-  div.max-w-min.mx-auto.gap-2
+div.mx-auto.mt-7.absolute(class="min-h-1")
+  div.mx-auto.gap-2
     button
-      NuxtLink.items-center.pt-5.px-2.py-2.text-sm.font-medium.rounded-md.cursor-pointer.no-border.relative(style="text-align:center;" class='text-gray-999 hover:text-black transition duration-300' to='/' active-class="active") Home
+      NuxtLink(t='/') Home
     button 
       NuxtLink(to='/my-dashboards') my dashboards
     button
       NuxtLink(to='/shared-dashboards') shared dashboards
     button
       NuxtLink(to='/new-dashboard') new dashboard
+    button
+      NuxtLink(to='/admin') admin
 </template>
 
